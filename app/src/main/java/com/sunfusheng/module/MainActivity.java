2baa9c42e16a6949b85a1b87f74eef01a5c87373
sfsheng0322 <sfsheng0322@gmail.com>
2016-07-07 11:07:45 +0800

    add two module

--- app/src/main/java/com/sunfusheng/module/MainActivity.java
+++ app/src/main/java/com/sunfusheng/module/MainActivity.java
@@ -1,13 +1,43 @@
 package com.sunfusheng.module;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.tv_weather)
+    TextView tvWeather;
+    @Bind(R.id.tv_phone)
+    TextView tvPhone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initListener();
+    }
+
+    private void initListener() {
+        tvWeather.setOnClickListener(this);
+        tvPhone.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.tv_weather:
+
+                break;
+            case R.id.tv_phone:
+
+                break;
+        }
     }
 }
